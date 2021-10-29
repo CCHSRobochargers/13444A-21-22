ab53da216c0c6bf8dcfacf290d945bbe1318297e
sw2017b <cchsrobochargers@gmail.com>
2021-10-28 21:33:23 -0400

    Here's hoping...

--- 13444A-1/src/main.cpp
+++ 13444A-1/src/main.cpp
@@ -8,13 +8,6 @@
 /*----------------------------------------------------------------------------*/
 
 // ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// liftMotors           motor_group   2, 9            
-// MotorGroup1          motor_group   1, 11           
-// MotorGroup10         motor_group   10, 20          
-// Drivetrain           drivetrain    3, 4            
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
@@ -23,35 +16,22 @@ using namespace vex;
 
 // A global instance of competition
 competition Competition;
-void  vexcodeInit( void ) {}
-// define your global instances of motors and other devices here
-// VEXcode device constructors
-brain Brain = brain();
-controller Controller1 = controller(primary);
-motor liftMotorsMotorA = motor(PORT2, ratio36_1, true);
-motor liftMotorsMotorB = motor(PORT9, ratio36_1, false);
-motor_group liftMotors = motor_group(liftMotorsMotorA, liftMotorsMotorB);
-motor MotorGroup1MotorA = motor(PORT1, ratio18_1, false);
-motor MotorGroup1MotorB = motor(PORT11, ratio18_1, true);
-motor_group MotorGroup1 = motor_group(MotorGroup1MotorA, MotorGroup1MotorB);
-motor MotorGroup10MotorA = motor(PORT10, ratio18_1, false);
-motor MotorGroup10MotorB = motor(PORT20, ratio18_1, true);
-motor_group MotorGroup10 = motor_group(MotorGroup10MotorA, MotorGroup10MotorB);
-drivetrain Drivetrain = drivetrain(MotorGroup1, MotorGroup10, 319.19, 406.4, 266.7, mm, 1);
 
-/*
+// define your global instances of motors and other devices here
+
 controller Controller1 = controller();
-motor leftDrive = motor(PORT1, false);
-motor rightDrive = motor(PORT10, true);
-motor leftDrive2 = motor(PORT11, true);
-motor rightDrive2 = motor(PORT20, false);
+motor leftDrive = motor(PORT1, true);
+motor leftDrive2 = motor(PORT11, false);
+motor rightDrive = motor(PORT10, false); 
+motor rightDrive2 = motor(PORT20, true);
 motor liftMotorLeft = motor(PORT2, true);
 motor liftMotorRight = motor(PORT9, false);
+motor grabMotor = motor(PORT19, false);
 motor_group liftMotors = motor_group(liftMotorLeft, liftMotorRight);
-*/
-int a = 50;
-bool ready = false;
-int autonType = 0;
+motor_group leftMotors = motor_group(leftDrive, leftDrive2);
+motor_group rightMotors = motor_group(rightDrive, rightDrive2);
+
+int a = 100;
 
 /*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
@@ -64,98 +44,63 @@ int autonType = 0;
 /*---------------------------------------------------------------------------*/
 
 void lowPower() {
-  a = 25;
-}
-
-void highPower() {
   a = 50;
 }
 
-/*
+void highPower() {
+  a = 100;
+}
+
+void grab() {
+  grabMotor.spinFor(1, seconds);
+}
+
+void resetGrab() {
+  grabMotor.setPosition(-25, degrees);
+}
+
 void driveInches(double inches) {
-  leftDrive.spinFor(directionType::fwd, (inches / (3.14 * 4)) * 360, rotationUnits::deg, false);
-  rightDrive.spinFor(directionType::fwd, (inches / (3.14 * 4)) * 360, rotationUnits::deg, false);
-  leftDrive2.spinFor(directionType::fwd, (inches / (3.14 * 4)) * 360, rotationUnits::deg, false);
-  rightDrive2.spinFor(directionType::fwd, (inches / (3.14 * 4)) * 360, rotationUnits::deg, true);
+  leftMotors.spinFor(directionType::fwd, (inches / (3.14 * 4)) * 360, rotationUnits::deg, false);
+  rightMotors.spinFor(directionType::fwd, (inches / (3.14 * 4)) * 360, rotationUnits::deg);
 }
 
 void turnDegrees(double degrees) {
-  leftDrive.spinFor(directionType::fwd, 360 * (3.14 * 18 / degrees), rotationUnits::deg, false);
-  rightDrive.spinFor(directionType::rev, 360 * (3.14 * 18 / degrees), rotationUnits::deg, false);
-  leftDrive2.spinFor(directionType::fwd, 360 * (3.14 * 18 / degrees), rotationUnits::deg, false);
-  rightDrive2.spinFor(directionType::rev, 360 * (3.14 * 18 / degrees), rotationUnits::deg, true);
-}
-*/
-void leftAuton() {
-  Drivetrain.turnFor(-15 / 4, degrees);
-  Drivetrain.driveFor(220, inches);
-  liftMotors.rotateFor(-40 * 5, degrees);
-  Drivetrain.driveFor(-22, inches);
-  Drivetrain.turnFor(15, degrees);
-  liftMotors.rotateFor(-30 * 5, rotationUnits::deg);
-  Drivetrain.driveFor(22, inches);
-  liftMotors.rotateFor(70 * 5, rotationUnits::deg);
-  Drivetrain.driveFor(8, inches);
-  liftMotors.rotateFor(-30 * 5, rotationUnits::deg);
-  Drivetrain.driveFor(2, inches);
-  liftMotors.rotateFor(-30 * 5, rotationUnits::deg);
-  Drivetrain.driveFor(24, inches);
+  leftMotors.spinFor(directionType::fwd, 360 * (3.14 * 18 / degrees), rotationUnits::deg, false);
+  rightMotors.spinFor(directionType::rev, 360 * (3.14 * 18 / degrees), rotationUnits::deg);
 }
 
-void rightAuton() {
-  drivetrain.turnFor(360, degrees);
-  Drivetrain.driveFor(12, inches);
-  Drivetrain.turnFor(45, degrees);
-  Drivetrain.driveFor(9, inches);
-  liftMotors.rotateFor(-40 * 15, rotationUnits::deg);
-  Drivetrain.driveFor(-9, inches);
-  liftMotors.rotateFor(-30 * 15, rotationUnits::deg);
-  Drivetrain.turnFor(20, degrees);
-  Drivetrain.driveFor(12, inches);
-  liftMotors.rotateFor(15 * 15, rotationUnits::deg);
-  Drivetrain.driveFor(-6, inches);
-  Drivetrain.turnFor(-65, degrees);
-  Drivetrain.driveFor(-4, inches);
-  liftMotors.rotateFor(-1 * 15, rotationUnits::deg);
-  Drivetrain.driveFor(-10, inches);
-}
+int autonType = 0;
 
 void pre_auton(void) {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-  Controller1.ButtonA.pressed(lowPower);
-  Controller1.ButtonB.pressed(highPower);
-
-  Brain.Screen.drawCircle(120, 136, 120);
-  Brain.Screen.drawCircle(360, 136, 120);
-
-  while (autonType == 0) {
-    
-    int touchX = Brain.Screen.xPosition();
-    int touchY = Brain.Screen.yPosition();
-
-    if (touchX < 240 && touchX > 0) {
-      autonType = 1;
-      Controller1.Screen.print("Left Autonomous");
-      Brain.Screen.clearScreen();
-      Brain.Screen.print("Go!");
-    }
-
-    if (touchX >= 240) {
-      autonType = 2;
-      Controller1.Screen.print("Right Autonomous");
-      Brain.Screen.clearScreen();
-      Brain.Screen.print("Go!");
-    }
-
-  Controller1.Screen.newLine();
-  Controller1.Screen.print("Go, Go, Go!!!");
-  Controller1.Screen.newLine();
-  Controller1.Screen.print("On an adventure!");
-
-  }
+  Controller1.ButtonDown.pressed(lowPower);
+  Controller1.ButtonRight.pressed(highPower);
+  Controller1.ButtonY.pressed(resetGrab);
+  Controller1.ButtonY.released(grab);
+  Brain.Screen.drawRectangle(0, 0, 240, 180, blue);
+  Brain.Screen.drawRectangle(240, 0, 240, 180, red);
+  Brain.Screen.drawRectangle(0, 180, 480, 60, green);
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
+  while (autonType == 0) {
+    int touchX = Brain.Screen.xPosition();
+    int touchY = Brain.Screen.yPosition();
+    if (touchX < 240 && touchX > 0 && touchY <= 180) {
+      autonType = 1;
+      Brain.Screen.clearScreen();
+      Brain.Screen.print("Left Auton");
+    } else if (touchX > 240 && touchY <= 180) {
+      autonType = 2;
+      Brain.Screen.clearScreen();
+      Brain.Screen.print("Right Auton");
+    } else if (touchY >= 180) {
+      autonType = 3;
+      Brain.Screen.clearScreen();
+      Brain.Screen.print("Skills Auton");
+    }
+    wait(50, msec);
+  }
 }
 
 /*---------------------------------------------------------------------------*/
@@ -173,10 +118,30 @@ void autonomous(void) {
   // Insert autonomous user code here.
   // ..........................................................................
   if (autonType == 1) {
-    leftAuton();
+    turnDegrees(-15);
+    driveInches(22);
+    liftMotors.spinFor(30 * 5, rotationUnits::deg);
+    driveInches(-10);
+    liftMotors.spinFor(30 * 5, rotationUnits::deg);
+    turnDegrees(-75);
+    driveInches(22);
+    liftMotors.spinFor(-5 * 5, rotationUnits::deg);
+    driveInches(-24);
+    liftMotors.spinFor(5 * 5, rotationUnits::deg);
   }
   if (autonType == 2) {
-    rightAuton();
+    driveInches(24);
+    liftMotors.spinFor(30 * 5, degrees);
+    driveInches(-20);
+    liftMotors.spinFor(30 * 5, degrees);
+    turnDegrees(15);
+    driveInches(26);
+    liftMotors.spinFor(-5 * 5, degrees);
+    driveInches(-26);
+    liftMotors.spinFor (5 * 5, degrees);
+  }
+  if (autonType == 3) {
+    
   }
 }
 
@@ -202,8 +167,8 @@ void usercontrol(void) {
     // update your motors, etc.
     // ........................................................................
     liftMotors.setStopping(brakeType::hold);
-    MotorGroup1.spin(directionType::fwd, Controller1.Axis3.value() * a / 100, velocityUnits::pct);
-    MotorGroup10.spin(directionType::rev, Controller1.Axis2.value() * a / 100, velocityUnits::pct);
+    leftMotors.spin(directionType::fwd, Controller1.Axis3.value() * a / 100, velocityUnits::pct);
+    rightMotors.spin(directionType::fwd, Controller1.Axis2.value() * a / 100, velocityUnits::pct);
     if (Controller1.ButtonL1.pressing()) {
       liftMotors.spin(directionType::fwd, 50, velocityUnits::pct);
     } else if (Controller1.ButtonL2.pressing()) {
--- 13444A-1/src/robot-config.cpp
+++ 13444A-1/src/robot-config.cpp
@@ -1,10 +1,15 @@
+#include "vex.h"
+
 using namespace vex;
 
-extern brain Brain;
+// A global instance of brain used for printing to the V5 brain screen
+brain Brain;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
  *
  * This should be called at the start of your int main function.
  */
-void vexcodeInit(void);
+void vexcodeInit(void) {
+  // Nothing to initialize
+}
